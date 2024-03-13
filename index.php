c90e8736e15b5d84c623abcad39b4acd53bb8f48
mursidi4321 <mursidi4321@gmail.com>
2024-03-14 05:13:16 +0700

    tambah file index

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coba Repo2</title>
+</head>
+<body>
+    <h1>Membuat Git repo di github</h1>
+</body>
+</html>
